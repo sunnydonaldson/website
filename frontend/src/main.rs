64fde9654aa47d423664f0826d44a225ae3b8360
Sunny <54858282+sunnydonaldson@users.noreply.github.com>
2023-01-29 12:44:17 +0000

    Set up basic frontend.

---
+++ frontend/src/main.rs
@@ -0,0 +1,14 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div>
+            <h1>{"Hello World!"}</h1>
+        </div>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
