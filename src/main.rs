c296e8526af4a3c8edca481dc4739521c6635f7c
Sunny <54858282+sunnydonaldson@users.noreply.github.com>
2023-02-19 15:23:19 +0000

    Add basic actix application.

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.01", 8080))?
+    .run()
+    .await
+}
